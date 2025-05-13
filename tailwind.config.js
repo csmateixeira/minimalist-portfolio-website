83e72c7749a4e571e77988896f5447708bcb373f
Carla T <csmateixeira@users.noreply.github.com>
2025-05-13 15:58:01 +0100

    Initial commit with Next.js configuration

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        extend: {
+            colors: {
+                white: "#fff",
+                black: "#121212",
+                tone0: "#222035",
+                tone1: "#373549",
+                tone2: "#95939f",
+                primary: "#827fe7",
+                surface0: "#8b8b8b"
+            }
+        },
+    },
+    plugins: [],
+}
+
